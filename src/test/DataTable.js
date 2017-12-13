d2f287162c30aefa1ae1b45eef096c4cd7da0f3b
gurpreetbirdi <birdi.gpsingh@gmail.com>
2017-12-13 19:41:46 +0530

    # table search added

--- src/test/DataTable.js
+++ src/test/DataTable.js
@@ -7,21 +7,60 @@ var products = [{
     id: 1,
     name: "Product1",
     price: 100
-}, {
+    }, {
     id: 2,
     name: "Product2",
     price: 80
     }, {
+        id: 3,
+        name: "Product3",
+        price: 180
+    },{
+        id: 4,
+        name: "Product4",
+        price: 380
+    }, {
+        id: 1,
+        name: "Product1",
+        price: 100
+    }, {
         id: 2,
         name: "Product2",
         price: 80
-    },
-    {
+    }, {
+        id: 3,
+        name: "Product3",
+        price: 180
+    }, {
+        id: 4,
+        name: "Product4",
+        price: 380
+    }, {
+        id: 1,
+        name: "Product1",
+        price: 100
+    }, {
         id: 2,
         name: "Product2",
         price: 80
+    }, {
+        id: 3,
+        name: "Product3",
+        price: 180
+    }, {
+        id: 4,
+        name: "Product4",
+        price: 380
     }
 ];
+class ActiveFormatter extends React.Component {
+    render() {
+        return (
+            <input type='checkbox' checked={this.props.active} />
+        );
+    }
+}
+
 class DataTable extends React.Component {
     constructor(props){
         super(props);
@@ -34,19 +73,37 @@ class DataTable extends React.Component {
             hideCol: prevState.hideCol ? false : true
         }));
     }
+    activeFormatter = (cell, row) => {
+        return (
+            <ActiveFormatter active={cell} />
+        );
+    }
+    onAfterDeleteRow = (rowKeys) => {
+        alert('The rowkey you drop: ' + rowKeys);
+    }
+    afterSearch = (searchText, result) => {
+        console.log('Your search text is ' + searchText);
+        console.log('Result is:');
+    }
     render(){
+        const options = {
+            paginationPosition: 'both',  // default is bottom, top and both is all available
+            afterDeleteRow: this.onAfterDeleteRow,  // A hook for after droping rows.
+            afterSearch: this.afterSearch  // define a after search hook
+        };
+        const selectRowProp = {
+            mode: 'checkbox'
+        };
         return(
             <div>
             <button onClick={this.handleClick}>hide col</button>
-                <BootstrapTable data={products} height='200' scrollTop={'Bottom'} striped hover>
-                    <TableHeaderColumn width='50' dataField='id' isKey dataSort={true}>ProductID</TableHeaderColumn>
-                    <TableHeaderColumn width='100' dataField='name' dataSort={true}>ProductName</TableHeaderColumn>
-                    <TableHeaderColumn width='50' dataField='price' hidden ={this.state.hideCol}>ProductPrice</TableHeaderColumn>
-                    <TableHeaderColumn width='50' dataField='name'>ProductName</TableHeaderColumn>
-                    <TableHeaderColumn width='100' dataField='price'>ProductPrice</TableHeaderColumn>
-                    <TableHeaderColumn width='150' dataField='name'>ProductName</TableHeaderColumn>
-                    <TableHeaderColumn width='150' dataField='price'>ProductPrice</TableHeaderColumn>
-                    <TableHeaderColumn width='150' dataField='name'>ProductName</TableHeaderColumn>
+            <BootstrapTable data={products} deleteRow={true} selectRow={selectRowProp} striped hover 
+                    pagination={true} options={options} search={true}>
+                    <TableHeaderColumn width='100' dataField='id' isKey dataSort={true} searchable={false} 
+                    hidden={this.state.hideCol}>ProductID</TableHeaderColumn>
+                    <TableHeaderColumn dataField='name' dataSort={true}>ProductName</TableHeaderColumn>
+                    <TableHeaderColumn dataField='price' dataSort={true}>ProductPrice</TableHeaderColumn>
+                    <TableHeaderColumn dataField='active' dataFormat={ this.activeFormatter }>Active</TableHeaderColumn>
                 </BootstrapTable>
             </div>
         );
