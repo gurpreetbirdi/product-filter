56e39e6b9b598ecb6baa4169ce180ff2dc36d223
Gurpreet Singh <gurpreet.singh1@agreeya.net>
2018-03-14 18:40:21 +0530

    # eslint error removed

--- src/test/Activity.js
+++ src/test/Activity.js
@@ -2,28 +2,28 @@ import React from 'react';
 import ReactSelect from './ReactSelect';
 
 class ActivityRow extends React.Component {
-constructor(props){
-  super(props);
-  this.state={
-    activityList:[
-        {name: 'BREAK DOWN TIME', value: '1'},
-        {name: 'CHANGE OVER', value: '2'},
-        {name: 'COOLING TANK CLEANING', value: '3'},
-        {name: 'DOWN TIME', value: '4'},
-        {name: 'PRODUCTION', value: '5'},
-        {name: 'PROFILE CORRECTION', value: '6'},
-        {name: 'PROFILE PLATE CLEANING', value: '7'},
-        {name: 'SCREW/ADOPTER CLEANING', value: '8'},
-        {name: 'SET UP', value: '9'},
-        {name: 'SIZER FAIL', value: '10'},
-        {name: 'TRAINING', value: '11'}
-    ]
-  };
-}
-handleChange=(e)=>{
-  this.props.onInputChange(e.target.name,e.target.value,this.props.rowIndex);
-  // console.log(e.target.name);
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      activityList: [
+        { name: 'BREAK DOWN TIME', value: '1' },
+        { name: 'CHANGE OVER', value: '2' },
+        { name: 'COOLING TANK CLEANING', value: '3' },
+        { name: 'DOWN TIME', value: '4' },
+        { name: 'PRODUCTION', value: '5' },
+        { name: 'PROFILE CORRECTION', value: '6' },
+        { name: 'PROFILE PLATE CLEANING', value: '7' },
+        { name: 'SCREW/ADOPTER CLEANING', value: '8' },
+        { name: 'SET UP', value: '9' },
+        { name: 'SIZER FAIL', value: '10' },
+        { name: 'TRAINING', value: '11' }
+      ]
+    };
+  }
+  handleChange = (e) => {
+    this.props.onInputChange(e.target.name, e.target.value, this.props.rowIndex);
+    // console.log(e.target.name);
+  }
   render() {
     return (
       <tr>
@@ -32,9 +32,9 @@ handleChange=(e)=>{
         <td>{<ReactSelect name="select1" options={this.state.activityList} value="9" />}</td>
         <td><input className="Uppercase" name="varAnly" type="text" onChange={this.handleChange} /></td>
         <td><input className="Uppercase" name="actTaken" type="text" onChange={this.handleChange} /></td>
-        <td><input type="button" onClick={this.props.onClickPlus} value="Add"/>
+        <td><input type="button" onClick={this.props.onClickPlus} value="Add" />
         </td>
-        <td><input type="button" onClick={this.props.onClickMinus} value="Remove"/>
+        <td><input type="button" onClick={this.props.onClickMinus} value="Remove" />
         </td>
       </tr>
     );
@@ -47,25 +47,25 @@ class ActivityTable extends React.Component {
     this.state = {
       activityNum: 1,
       activityRow: [<ActivityRow rowIndex={0} onInputChange={this.handleInputChange}
-        onClickPlus = {this.handleAddNewActClick} key = {1} />],
-      data:[{
-        from1:"",to1:"", select1:"",varAnly:"", actTaken:""
+        onClickPlus={this.handleAddNewActClick} key={1} />],
+      data: [{
+        from1: "", to1: "", select1: "", varAnly: "", actTaken: ""
       }]
     };
   }
-  handleInputChange=(name,value1,rwIndex)=>{
-    this.setState(prevState=>{
+  handleInputChange = (name, value1, rwIndex) => {
+    this.setState(prevState => {
       let newData = prevState.data
-      newData[rwIndex][name]= value1;
-      return{data:newData};
+      newData[rwIndex][name] = value1;
+      return { data: newData };
     });
   }
-  componentDidUpdate=()=>{
+  componentDidUpdate = () => {
     console.log(this.state.data);
   }
 
-  handleRemoveNewAct=()=>{
-    let removeAct= this.state.activityRow;
+  handleRemoveNewAct = () => {
+    let removeAct = this.state.activityRow;
     removeAct.pop();
     this.setState({
       activityNum: this.state.activityNum - 1,
@@ -78,14 +78,14 @@ class ActivityTable extends React.Component {
     let newData = this.state.data;
     newAct.push(<ActivityRow rowIndex={newAct.length} onInputChange={this.handleInputChange}
       onClickPlus={this.handleAddNewActClick}
-      onClickMinus = {this.handleRemoveNewAct} key={this.state.activityNum + 1} />);
+      onClickMinus={this.handleRemoveNewAct} key={this.state.activityNum + 1} />);
     newData.push({
-      from1:"",to1:"", select1:"",varAnly:"", actTaken:""
+      from1: "", to1: "", select1: "", varAnly: "", actTaken: ""
     });
     this.setState({
       activityNum: this.state.activityNum + 1,
       activityRow: newAct,
-      data:newData
+      data: newData
     });
     console.log("added");
   }
@@ -103,7 +103,14 @@ class ActivityTable extends React.Component {
                 <th>Action Taken</th>
               </tr>
             </thead>
-            <tbody>{this.state.activityRow}<tr><td><input type="button" value="Submit" /></td></tr></tbody>
+            <tbody>
+              {this.state.activityRow}
+              <tr>
+                <td>
+                  <input type="button" value="Submit" />
+                </td>
+              </tr>
+            </tbody>
           </table>
         </form>
       </div>
